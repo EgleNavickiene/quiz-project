b94f8b7fdfdaf4043c99ab32f47db338af3599e2
EgleNavickiene <navickiene@gmail.com>
2021-11-22 20:23:57 +0200

    bootstrap versijos pakeitimas (rankiniu būdu)
    ir pradinio vaizdo dėliojimas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-project';
+
+  // public title = 'quiz';
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ProgressbarModule.forRoot(),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
