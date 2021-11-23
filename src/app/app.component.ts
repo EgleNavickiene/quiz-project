5d0b567676126fc5a3114e9aec630596eecac34d
EgleNavickiene <navickiene@gmail.com>
2021-11-23 19:40:48 +0200

    frontend dalies tęsinys
    Atsakymų cards
    Bottom dalis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'quiz-project';
 
-  // public title = 'quiz';
+  public title = 'quiz';
+
+  public answers : Array<any> = [
+    "Atsakymas 1",
+    "Atsakymas 2",
+    "Atsakymas 3",
+    "Atsakymas 4",
+  ];
+
+  constructor() {
+
+  }
 
 
 }
